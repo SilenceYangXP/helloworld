6f1798b7235cddcbfa1818b28077465a805eb7d5
yxp <xpyangf@163.com>
2017-08-11 17:13:59 +0800

    complete method3 and modify method1 and method2

--- hello.java
+++ hello.java
@@ -1,14 +1,14 @@
 public class Hello
 {
   public static void method1(){
-  logger.info("method1");
+  logger.info("method1-1");
   }
 
   public static void method2(){
-  logger.info("method2");
+  logger.info("method2-1");
   }
   public static void method3(){
-  
+  logger.info("method3");
   }
 
 
